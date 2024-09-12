d8b249fdcd61ebbccc615dc901021b976d87588b
Jefferson Felipe <jeffersons@petribusa.com.br>
2024-09-12 16:00:42 -0300

    changed: criacao da rota de update dos dados

--- BACKEND/src/routes.ts
+++ BACKEND/src/routes.ts
@@ -1,41 +1,44 @@
+//Importando fastify_
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+
+//Importando os controladores_
 import { CreateUserController } from "./Controllers/CreateUserController";
 import { ListUsersController } from "./Controllers/ListUsersControllers";
-import joi from 'joi';
+import { DeleteUserController } from "./Controllers/DeleteUserController";
+import { UpdateUserController } from "./Controllers/UpdateUserController";
 
-
-const schema = joi.object({
-    name: joi.string().min(3).max(45).required().messages({
-        
-    }),
-    email: joi.string().email().required(),
-
-})
-
-const UserSchema = {
-    body:{
-        type: 'object',
-        required: ['name','email', 'cpf', 'age', 'password'],
-        properties: {
-            name: {type: 'string', minLength: 3},
-            email: {type: 'string', format: 'email'},
-            cpf: {type: 'string', minLength: 11},
-            age: {type: 'number'},
-            password: {type: 'string', minLength: 5},
-        },
-    }
-}
+//Validação usando a propriedade Joi_
+import schema from "../Validate/ValidateCreate";
+import schemaUpdate from "../Validate/ValidateUpdate";
 
 export const routes = async (fastify:FastifyInstance, options: FastifyPluginOptions) => {
-    fastify.post('/create',{
-        schema: UserSchema
-    }, (request: FastifyRequest, reply: FastifyReply) => {
+    //Rota para criar um usuário_
+    fastify.post('/create',(request: FastifyRequest, reply: FastifyReply) => {
+        const {error} = schema.validate(request.body);
+        if(error) return reply.status(400).send({error: error.details[0].message});
+        
         const createService = new CreateUserController();
         return createService.CreateUser(request, reply);
     });
 
+    //Rota para Listar os usuários_
     fastify.get('/listUsers', (request: FastifyRequest, reply: FastifyReply) => {
         const listUsers = new ListUsersController();
         return listUsers.ListUsers(request, reply);
+    });
+
+    //Rota para deletar um usuário específico_
+    fastify.delete('/api/:id/delete/',(request:FastifyRequest, reply:FastifyReply) => {
+        const DeleteUser = new DeleteUserController();
+        return DeleteUser.DeleteUser(request, reply);
+    });
+
+    //Rota para atualizar um usuário específico_
+    fastify.put('/api/:id/update/',(request:FastifyRequest, reply:FastifyReply) => {
+        const {error} = schemaUpdate.validate(request.body);
+        if(error) return reply.status(400).send({error: error.details[0].message});
+        
+        const update = new UpdateUserController();
+        return update.Update(request,reply);
     });
 }
