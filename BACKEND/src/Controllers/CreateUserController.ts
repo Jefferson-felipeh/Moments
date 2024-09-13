abf5390002c45b0206bbbf281473edb32a3bb8c1
Jefferson Felipe <jeffersons@petribusa.com.br>
2024-09-13 15:42:43 -0300

    changed: Atualização do controlador de criãção de usuarios

--- BACKEND/src/Controllers/CreateUserController.ts
+++ BACKEND/src/Controllers/CreateUserController.ts
@@ -5,6 +5,8 @@ export class CreateUserController{
     CreateUser = async (request: FastifyRequest, reply: FastifyReply) => {
         const {name, email, cpf, age, password} = request.body as {name: string, email: string, cpf: string, age: number, password:string};
 
+        if(!name || !email || !cpf || !age || !password) return reply.status(400).send('Error!');
+
         const createService = new CreateUserService();
         const create = await createService.execute({name, email, cpf, age, password});
         
