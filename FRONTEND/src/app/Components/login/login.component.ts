2ed59074842bb3631963978b6ccffe2f86047bfe
Jefferson Felipe <jeffersons@petribusa.com.br>
2024-09-19 15:45:27 -0300

    fazendo a validação dos dados no componente

--- FRONTEND/src/app/Components/login/login.component.ts
+++ FRONTEND/src/app/Components/login/login.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { api } from '../../service/api';
+import { dataUser, User } from '../../interfaces/dataUser';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  
+export class LoginComponent implements OnInit{
+  datasList:dataUser[] = [];
+  emailExists!:boolean;
 
-  EventClickedBtn = async (e:Event) => {
-    e.preventDefault();
+  datasUserForm:User = {
+    email: ''
+  }
+  validation:boolean = false;
+
+  ngOnInit(): void {}
+
+  EventClickedBtn = async ($event:Event) => {
+    $event.preventDefault();
+
+    this.datasList = (await api.get('/listUsers')).data;
+    
+    for(let i:number = 0;i< this.datasList.length;i++) this.datasList[i].email.toLowerCase() == this.datasUserForm.email.toLowerCase() ? this.emailExists = true: this.emailExists == false;
+   
   }
 }
