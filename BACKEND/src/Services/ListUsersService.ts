a9e195c13f45babec7ca6b73c6ae4c5274136244
Jefferson Felipe <jeffersons@petribusa.com.br>
2024-09-27 11:57:39 -0300

    changed

--- BACKEND/src/Services/ListUsersService.ts
+++
@@ -1,8 +0,0 @@
-import prismaClient from "../prisma";
-
-export class ListUsersService{
-    execute = async () => {
-        const list = await prismaClient.user.findMany();
-        return list;
-    }
-}
