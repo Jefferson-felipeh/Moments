7ce47980070cc5296064b11804556170689cc012
Jefferson Felipe <jeffersons@petribusa.com.br>
2024-11-14 16:39:44 -0300

    atualização do banco de dados

---
+++ BACKEND/prisma/migrations/20241030120518_update_users/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN "city" TEXT;
+ALTER TABLE "users" ADD COLUMN "confirmPassword" TEXT;
+ALTER TABLE "users" ADD COLUMN "num" TEXT;
+ALTER TABLE "users" ADD COLUMN "rua" TEXT;
+ALTER TABLE "users" ADD COLUMN "uf" TEXT;
